734f04b0d68e60c9d80dff1e7c263a2818605365
Luke <luke.li@NY1-MJWFG44.owg.ds.corp>
2013-07-22 16:57:12 -0400

    Made it work finally; have to figure out how to get <p> to turn into new lines

--- NaverStoryParser/com/ll2585/parser.py
+++ NaverStoryParser/com/ll2585/parser.py
@@ -1,16 +1,31 @@
 import sys
 from bs4 import BeautifulSoup
+import html2text
 from urllib.request import urlopen
+import types
 
-#url="http://novel.naver.com/webnovel/detail.nhn?novelId=63068&volumeNo=1&week=THU"
+
+#url="http://novel.naver.com/webnovel/detail.nhn?novelId=1&volumeNo=55&week=MON"
 #page=urlopen(url)
 #soup = BeautifulSoup(page.read())
+def replace_with_newlines(element):
+    text = ''
+    for elem in element.recursiveChildGenerator():
+        if isinstance(elem, types.StringTypes):
+            text += elem.strip()
+        elif elem.name == 'p':
+            text += '\n'
+    return text
 
-#soup = BeautifulSoup(open("naver4.html"))
-soup = BeautifulSoup(u'\xa0')
+soup = BeautifulSoup(open('naver4.html', encoding="utf-8"))
+#soup = BeautifulSoup(u'\xa0')
 #temp=soup.findAll("div",{"id":"detail_view_container"})
 #print(page.read().decode("utf8"))
 
 #a_file = open('wtf.txt', encoding="utf-8")
-print(soup.encode('utf-8'))
-#print (u'哈哈'.encode('gb2312'))
+for e in soup.find_all('div', attrs={'class':'detail_view_content ft15'}):
+    spans = soup.find_all('span')
+    for span in spans:
+        span.decompose()
+    print(e.get_text())
+
